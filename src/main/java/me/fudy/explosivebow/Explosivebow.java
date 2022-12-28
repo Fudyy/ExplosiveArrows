02255c86ca3af2c1481c45d06ff38d3a1c8a6607
Fudy <carlosgonsalazar@gmail.com>
2022-12-27 21:33:18 -0300

    Basic config for the project.

---
+++ src/main/java/me/fudy/explosivebow/Explosivebow.java
@@ -0,0 +1,21 @@
+package me.fudy.explosivebow;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Explosivebow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getServer().sendMessage(Component.text("Explosive arrows have been initialized!").color(TextColor.color(0,255,0)));
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        Bukkit.getServer().sendMessage(Component.text("Explosive arrows is shutting down!").color(TextColor.color(255,0,0)));
+    }
+}
